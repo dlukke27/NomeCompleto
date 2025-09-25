1a87d7b8c57c44be963a2c7d554c10e81931b643
Dlukke27 <lukeechan12@gmail.com>
2025-09-24 21:27:49 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Digite seu primeiro nome: ");
+string Nome = Console.ReadLine();
+
+Console.Write("Digite seu sobrenome: ");
+string Sobrenome = Console.ReadLine();
+
+Console.WriteLine($"Olá {Nome} {Sobrenome}");
+
